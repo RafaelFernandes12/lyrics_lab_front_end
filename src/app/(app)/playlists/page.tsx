9bf303efa6b4757dd6520c959a04f7e637862c08
matheus07h <matheushenriquefs07@gmail.com>
2024-05-26 15:02:05 -0300

    feat: design of the read-all-playlists page done

--- src/app/(app)/playlists/page.tsx
+++ src/app/(app)/playlists/page.tsx
@@ -1,7 +1,24 @@
+import { PlaylistCard } from '@/components/PlaylistCard/PlaylistCard';
+import AddIcon from '@mui/icons-material/Add';
+
 export default function Playlists() {
   return (
-    <div>
-      <p>plasylists</p>
-    </div>
-  )
+    <section>
+      <section className="flex items-center justify-between">
+        <h1 className="text-3xl font-semibold">Playlists</h1>
+        <button className="flex items-center gap-2 rounded-xl bg-black p-3 text-white ">
+          <AddIcon />
+          <span>Adicionar playlist</span>
+        </button>
+      </section>
+      <section>
+        <ul className='flex items-center gap-8'>
+          <li><PlaylistCard /></li>
+          <li><PlaylistCard /></li>
+          <li><PlaylistCard /></li>
+          <li><PlaylistCard /></li>
+        </ul>
+      </section>
+    </section>
+  );
 }
