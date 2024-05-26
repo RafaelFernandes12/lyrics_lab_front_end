b5dba2fec0b630ec51d01ff9fb40caed5d157778
matheus07h <matheushenriquefs07@gmail.com>
2024-05-26 14:33:25 -0300

    feat: design of the read-song page done

---
+++ src/app/song/[id]/page.tsx
@@ -0,0 +1,37 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import Board from '../components/Board';
+
+interface IdProps {
+  params: {
+      id: string
+  }
+}
+
+export default async function Song({ params }: IdProps) {
+  // const response = await api.get(`/songs/${params.id}`);
+  // const song: songProps = response.data;
+
+  return (
+    <section>
+      <div className="flex items-center justify-between mb-5">
+        <h2 className="text-3xl font-semibold">Nome da música</h2>
+        <button className='bg-black text-white p-2 rounded-md'>Baixar PDF <PictureAsPdfIcon /></button> 
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-10">
+          <h3 className="text-lg">Tom: Cm7</h3>
+          <p>Cifra</p>
+        </div>
+        <div className="flex items-center justify-between gap-10">
+          <p>Modo</p>
+          <button><DeleteIcon /></button>
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <Board />
+      </div>
+    </section>
+  );
+}
---
+++ src/app/song/components/Board.tsx
@@ -0,0 +1,5 @@
+export default function Board() {
+  return(
+    <p className="font-black">Board</p>
+  );
+}
