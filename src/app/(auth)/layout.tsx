106cea0091a5ac374df4d28c7143a75332da07b2
RafaelFernandes12 <rfsilva@gmail.com>
2025-02-23 18:19:24 -0300

    fix: conserta bug de switchTheme apos logout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <body className="bg-[#FAFAFA]">
+    <body className="m-0 bg-slate-100 dark:bg-black">
       <AuthHeader />
       <main className="m-[6%] my-10 h-fit rounded-md">{children}</main>
     </body>
