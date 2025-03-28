3cc2f9e5285288a4538868ac45df8184c2dae5ba
Ramon-Mateus <ramonmateus00@gmail.com>
2025-03-28 11:34:30 -0300

    ajustes de lint

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -84,17 +84,15 @@ export async function login(email: string, password: string) {
       email,
       password,
     })
-    //const data = response.data
-    //return data
+    // const data = response.data
+    // return data
 
-    const { user, jwt } = response.data.value;
+    const { user, jwt } = response.data.value
 
     if (jwt) {
-      localStorage.setItem('jwt', jwt);
+      localStorage.setItem('jwt', jwt)
     }
-    
-    return { user, token: jwt };
-
+    return { user, token: jwt }
   } catch (error: any) {
     throw new Error(error || 'Erro na requisição.')
   }
